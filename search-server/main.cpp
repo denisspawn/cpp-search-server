76b32e5214e97faa510a0a65f33bdc1c2fac70fc
denisspawn <denisgaiciuk@gmail.com>
2022-05-14 16:25:13 +0300

    code correction of 2 spring

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -341,31 +341,54 @@ void TestExcludeMinusWordsFromSearchResults() {
         SearchServer server;
         server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
         ASSERT_HINT(server.FindTopDocuments("black dog -big"s).empty(), "Documents which contain minus words must be exclude from documents"s);
+        ASSERT_HINT(!server.FindTopDocuments("black dog"s).empty(), "Document should be found by request without minus words"s);
+    }
+}
+
+void TestDocumentsCount() {
+    const int doc_id = 60;
+    const string content = "huge gray owl"s;
+    const vector<int> ratings = {2, 4, -1};
+    {
+        SearchServer server;
+        ASSERT_HINT(server.GetDocumentCount() == 0, "Count of documents by default must be zero"s);
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        ASSERT_HINT(server.GetDocumentCount() > 0, "Count of documents must be not zero before adding of document"s);
+
     }
 }
 
 void TestMatchingDocumentBySearchRequest() {
     const int doc_id = 33;
     const string minus_word = " -small"s;
+    const string stop_words = "in on and"s;
     const string content = "small white cat"s;
     const vector<int> ratings = {4, 7, -8};
     const string raw_query = "white cat"s;
+    const DocumentStatus added_status = DocumentStatus::ACTUAL;
     
     {
         SearchServer server;
-        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        server.AddDocument(doc_id, content, added_status, ratings);
         const auto [words, status] = server.MatchDocument(raw_query, doc_id);
-        ASSERT_HINT(words.size() > 0, "Vector of matched words must be not empty");
+        ASSERT_HINT(words.size() > 0, "Vector of matched words must be not empty"s);
+        ASSERT_HINT(status == added_status, "Status of matched document must be equal to added document"s);
         vector<string> raw_query_words = SplitIntoWords(raw_query);
         sort(raw_query_words.begin(), raw_query_words.end());
-        ASSERT_EQUAL_HINT(words.size(), raw_query_words.size(), "Count of matched words should be equal to count of raw query");
+        ASSERT_HINT(words == raw_query_words, "Vectors of matching and querying should be equal"s);
     }
-    
+
     {
         SearchServer server;
-        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        server.SetStopWords(stop_words);
+        server.AddDocument(doc_id, content, added_status, ratings);
         const auto [words, status] = server.MatchDocument(raw_query + minus_word, doc_id);
-        ASSERT_HINT(words.size() == 0, "Vector of matching word should not contain any words if document contains minus words");
+        ASSERT_HINT(words.empty(), "Vector of matching word should not contain any words if document contains minus words"s);
+
+        vector<string> splitted_stop_words = SplitIntoWords(stop_words);
+        for (const string& stop_word : splitted_stop_words) {
+            ASSERT_HINT(!count(words.begin(), words.end(), stop_word), "Matched document should not have any stop word"s);
+        }
     }
 }
 
@@ -378,7 +401,7 @@ void TestRelevanceSortingDescOrder() {
     for (unsigned i = 0; i < documents.size(); ++i) {
         unsigned j = i + 1;
         if (j < documents.size()) {
-            ASSERT_HINT(documents[i].relevance > documents[j].relevance, "All documents must be sorted by descending of rating");
+            ASSERT_HINT(documents[i].relevance > documents[j].relevance, "All documents must be sorted by descending of rating"s);
         }
     }
 }
@@ -396,7 +419,7 @@ void TestAverageRatingResult() {
         for (const int rating : ratings) {
             rating_sum += rating;
         }
-        ASSERT_EQUAL_HINT((rating_sum / static_cast<int>(ratings.size())), found_docs[0].rating, "Average rating calculates wrong");
+        ASSERT_EQUAL_HINT((rating_sum / static_cast<int>(ratings.size())), found_docs[0].rating, "Average rating calculates wrong"s);
     }   
 }
 
@@ -415,31 +438,81 @@ void TestPredicateFunction() {
 }
 
 void TestFilterByStatus() {
-    SearchServer server;
-    server.AddDocument(0, "a white cat and a fashionable collar"s, DocumentStatus::ACTUAL, {8, -3});
-    server.AddDocument(1, "fluffy cat fluffy tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
-    server.AddDocument(2, "kind dog expressive eyes"s, DocumentStatus::BANNED, {5, -12, 2, 1});
     const string raw_query = "fluffy kind cat"s;
-    const auto documents = server.FindTopDocuments(raw_query, DocumentStatus::BANNED);
-    ASSERT(documents.size() == 1);
-    const auto [words, status] = server.MatchDocument(raw_query, documents[0].id);
-    ASSERT_HINT(status == DocumentStatus::BANNED, "Documents don't filtered by status");
+    {
+        SearchServer server;
+        server.AddDocument(0, "a white cat and a fashionable collar"s, DocumentStatus::ACTUAL, {8, -3});
+        server.AddDocument(1, "fluffy cat fluffy tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
+        server.AddDocument(2, "kind dog expressive eyes"s, DocumentStatus::BANNED, {5, -12, 2, 1});
+
+        const auto documents = server.FindTopDocuments(raw_query, DocumentStatus::BANNED);
+        ASSERT(documents.size() == 1);
+
+        const auto [words, status] = server.MatchDocument(raw_query, documents[0].id);
+        ASSERT_HINT(status == DocumentStatus::BANNED, "Documents don't filtered by status"s);
+
+        const auto not_existed_doc = server.FindTopDocuments(raw_query, DocumentStatus::REMOVED);
+        ASSERT_HINT(!documents.empty(), "Documents must be empty if given wrong status"s);
+
+    }
 }
 
 void TestCorrectRelevanceDocument() {
+    const int first_doc_id = 0;
+    const string first_doc_content = "a black cat and a fashionable collar"s;
+    const vector<int> first_doc_ratings = {8, -3};
+    const int second_doc_id = 3;
+    const string second_doc_content = "white dog expressive eyes"s;
+    const vector<int> second_doc_ratings = {9, -7};
+    //const string stop_words = "on in a and"s;
     const string raw_query = "white cat"s;
-    const string stop_words = "on in a and"s;
+
+    map<string, map<int, double>> word_to_document_freqs;
+
+    map<int, vector<string>> doc_id_to_split_words;
+
     
     {
         SearchServer server;
-        server.SetStopWords(stop_words);
+        //server.SetStopWords(stop_words);
 
-        server.AddDocument(0, "a black cat and a fashionable collar"s, DocumentStatus::ACTUAL, {8, -3});
-        server.AddDocument(3, "white dog expressive eyes"s,            DocumentStatus::ACTUAL, {9});
+        server.AddDocument(first_doc_id, first_doc_content, DocumentStatus::ACTUAL, first_doc_ratings);
+        server.AddDocument(second_doc_id, second_doc_content, DocumentStatus::ACTUAL, second_doc_ratings);
 
         const auto documents = server.FindTopDocuments(raw_query);
         
-        ASSERT_EQUAL_HINT(documents[0].relevance, documents[1].relevance, "Relevance of documents calculated incorrect");
+        const vector<string> first_doc_words = SplitIntoWords(first_doc_content);
+        doc_id_to_split_words[first_doc_id] = first_doc_words;
+        
+        const vector<string> second_doc_words = SplitIntoWords(second_doc_content);
+        doc_id_to_split_words[second_doc_id] = second_doc_words;
+
+        for (const auto& [doc_id, split_words] : doc_id_to_split_words) {
+            const double inv_word_count = 1.0 / split_words.size();
+            for (const string& word : split_words) {
+                word_to_document_freqs[word][doc_id] += inv_word_count;
+            }
+        }
+
+        const vector<string> query_words = SplitIntoWords(raw_query);
+        map<int, double> document_to_relevance;
+        for (const string& q_word : query_words) {
+            if (word_to_document_freqs.count(q_word) == 0) {
+                continue;
+            }
+            const double inverse_document_freq = log(doc_id_to_split_words.size() * 1.0 / word_to_document_freqs.at(q_word).size());
+            for (const auto [document_id, term_freq] : word_to_document_freqs.at(q_word)) {
+                document_to_relevance[document_id] += term_freq * inverse_document_freq;
+            }
+        }
+
+        Document server_document;
+        for (const auto& document : documents) {
+            if (document.id == first_doc_id)
+                server_document = document;
+        }
+
+        ASSERT_HINT(abs(document_to_relevance.at(first_doc_id) - server_document.relevance) < PRECISION, "Relevance of documents calculated incorrect"s);
     }
 }
 
@@ -447,6 +520,7 @@ void TestCorrectRelevanceDocument() {
 void TestSearchServer() {
     RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
     RUN_TEST(TestExcludeMinusWordsFromSearchResults);
+    RUN_TEST(TestDocumentsCount);
     RUN_TEST(TestMatchingDocumentBySearchRequest);
     RUN_TEST(TestRelevanceSortingDescOrder);
     RUN_TEST(TestAverageRatingResult);
