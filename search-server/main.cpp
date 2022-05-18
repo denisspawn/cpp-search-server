ad4f8c938cabee6c9bc5552b271b56043136a783
denisspawn <denisgaiciuk@gmail.com>
2022-05-18 10:44:58 +0300

    code rorrection of 3 sprint

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <numeric>
 #include <cmath>
 #include <iostream>
 #include <map>
@@ -45,7 +46,18 @@ vector<string> SplitIntoWords(const string& text) {
 
     return words;
 }
-    
+
+template <typename StringContainer>
+set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    set<string> non_empty_strings;
+    for (const string& str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.insert(str);
+        }
+    }
+    return non_empty_strings;
+}
+
 struct Document {
     Document() = default;
 
@@ -70,8 +82,14 @@ enum class DocumentStatus {
 class SearchServer {
 public:
     template <typename StringContainer>
-    explicit SearchServer(const StringContainer& stop_words)
-        : stop_words_(MakeUniqueNonEmptyStrings(stop_words)) {}
+    explicit SearchServer(const StringContainer& stop_words) {
+        set<string> non_empty_stop_words = MakeUniqueNonEmptyStrings(stop_words);
+        for (const string& word : stop_words) {
+            if (!IsValidWord(word))
+                throw invalid_argument("Words should not contain deprecated characters [0, 31]"s);
+            stop_words_ = non_empty_stop_words;
+        }
+    }
 
     explicit SearchServer(const string& stop_words_text)
         : SearchServer(SplitIntoWords(stop_words_text)) {}   
@@ -83,11 +101,7 @@ public:
         if ((document_id < 0) || (documents_.count(document_id) > 0)) {
             throw invalid_argument("The document ID must not be less than zero and the document ID must not match the one already added"s);
         }
-        vector<string> words;
-        if (!SplitIntoWordsNoStop(document, words)) {
-            throw invalid_argument("Words should not contain forbidden characters [0, 31]"s);
-        }
-
+        vector<string> words = SplitIntoWordsNoStop(document);
         const double inv_word_count = 1.0 / words.size();
         for (const string& word : words) {
             word_to_document_freqs_[word][document_id] += inv_word_count;
@@ -101,10 +115,7 @@ public:
     template <typename DocumentPredicate>
     vector<Document> FindTopDocuments(const string& raw_query, 
                                                 DocumentPredicate document_predicate) const {
-        Query query;
-        if (!ParseQuery(raw_query, query)) {
-            throw invalid_argument("String of query contains forbidden characters [0, 31] or '--' or empty string after '-'"s);
-        }
+        Query query = ParseQuery(raw_query);
         auto matched_documents = FindAllDocuments(query, document_predicate);
 
         sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
@@ -145,11 +156,8 @@ public:
     }
 
     tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query,
-                                                                  int document_id) const {
-        Query query;
-        if (!ParseQuery(raw_query, query)) {
-            throw invalid_argument("String of query contains forbidden characters [0, 31] or '--' or empty string after '-'"s);
-        }
+                                                        int document_id) const {
+        Query query = ParseQuery(raw_query);
         vector<string> matched_words;
         for (const string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
@@ -194,43 +202,23 @@ private:
         });
     }
     
-    template <typename StringContainer>
-    set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-        set<string> non_empty_strings;
-        for (const string& str : strings) {
-            if (!str.empty()) {
-                if (!IsValidWord(str))
-                    throw invalid_argument("Words should not contain forbidden characters [0, 31]"s);
-                non_empty_strings.insert(str);
-            }
-        }
-        return non_empty_strings;
-    }
-
-    [[nodiscard]] bool SplitIntoWordsNoStop(const string& text,
-                                            vector<string>& result) const {
-        result.clear();
+    vector<string> SplitIntoWordsNoStop(const string& text) const {
         vector<string> words;
         for (const string& word : SplitIntoWords(text)) {
-            if (!IsValidWord(word)) {
-                return false;
-            }
+            if (!IsValidWord(word))
+                throw invalid_argument("Words should not contain forbidden characters [0, 31]"s);
             if (!IsStopWord(word)) {
                 words.push_back(word);
             }
         }
-        result.swap(words);
-        return true;
+        return words;
     }   
 
     static int ComputeAverageRating(const vector<int>& ratings) {
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
         return rating_sum / static_cast<int>(ratings.size());
     }
     
@@ -240,24 +228,17 @@ private:
         bool is_stop;
     };
     
-    [[nodiscard]] bool ParseQueryWord(string text, QueryWord& result) const {
-        // Empty result by initializing it with default constructed QueryWord
-        result = {};
-
-        if (text.empty()) {
-            return false;
-        }
+    QueryWord ParseQueryWord(string text) const {
         bool is_minus = false;
-        if (text[0] == '-') {
+        if (!text.empty() && text[0] == '-') {
             is_minus = true;
             text = text.substr(1);
         }
         if (text.empty() || text[0] == '-' || !IsValidWord(text)) {
-            return false;
+            throw invalid_argument("A word of query contains forbidden characters [0, 31] or '--' or empty string after '-'"s);
         }
 
-        result = QueryWord{text, is_minus, IsStopWord(text)};
-        return true;
+        return {text, is_minus, IsStopWord(text)};
     }
 
     struct Query {
@@ -265,23 +246,22 @@ private:
         set<string> minus_words;
     };
     
-    [[nodiscard]] bool ParseQuery(const string& text, Query& result) const {
-        // Empty result by initializing it with default constructed Query
-        result = {};
+    Query ParseQuery(const string& text) const {
+        if (text.empty()) {
+            throw invalid_argument("String of query is epmty"s);
+        }
+        Query query;
         for (const string& word : SplitIntoWords(text)) {
-            QueryWord query_word;
-            if (!ParseQueryWord(word, query_word)) {
-                return false;
-            }
+            QueryWord query_word = ParseQueryWord(word);
             if (!query_word.is_stop) {
                 if (query_word.is_minus) {
-                    result.minus_words.insert(query_word.data);
+                    query.minus_words.insert(query_word.data);
                 } else {
-                    result.plus_words.insert(query_word.data);
+                    query.plus_words.insert(query_word.data);
                 }
             }
         }
-        return true;
+        return query;
     }
 
     // Existence required
